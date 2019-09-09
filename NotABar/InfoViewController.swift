9381808d47c447f81ff7eb2bdc445a11c6352a66
Josh Lytle <josh.lytle@MBP-12522.local>
2019-09-09 09:37:02 -0600

    final release

---
+++ NotABar/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  NotABar
+//
+//  Created by Josh Lytle on 2/17/19.
+//  Copyright © 2019 SoggyMop LLC. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
